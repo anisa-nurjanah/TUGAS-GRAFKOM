9044c2ac69149140c188b304ae111091ad223f06
dwifristanti <39585289+dwifristanti@users.noreply.github.com>
2019-11-20 10:24:24 +0700

    Create EnemyMove.cs

---
+++ EnemyMove.cs
@@ -0,0 +1,26 @@
+using System.Collection;
+using System.Collection.Generics;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    public int EnemySpeed;
+    public int XMoveDirection;
+    //update is called once per frame
+    void Update(){
+        RaycastHit2D hit = Physics2D.Raycast (transform.position,new Vector2 (XMoveDirection,0));
+        gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (XMoveDirection,0) * EnemySpeed;
+        if(hit.distance < 0.7f){
+            Flip ();
+        }
+    }
+    void Flip(){
+        if (XMoveDirection > 0){
+            XMoveDirection = -1;
+        } else
+        {
+            XMoveDirection = 1;
+        }
+    }
+}
